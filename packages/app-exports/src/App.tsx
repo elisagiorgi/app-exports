e84254d64c84325c2e5d244cad158990edd8ab5c
Giuseppe Ciotola <30926550+gciotola@users.noreply.github.com>
2023-05-30 16:51:07 +0200

    chore: disable devMode

--- packages/app-exports/src/App.tsx
+++ packages/app-exports/src/App.tsx
@@ -28,7 +28,7 @@ function App(): JSX.Element {
         domain={window.clAppConfig.domain}
         reauthenticateOnInvalidAuth={!isDev}
         loadingElement={<PageSkeleton />}
-        devMode
+        devMode={isDev}
       >
         <CoreSdkProvider>
           <Router base={basePath}>

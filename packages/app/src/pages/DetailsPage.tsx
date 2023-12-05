1bdbe8f23f5fee08ce37db8e00e6e1fb19d0cc5e
Elisa Giorgi <e.giorgi@adiacent.com>
2023-12-05 17:32:04 +0100

    fix: edit project name

--- packages/app/src/pages/DetailsPage.tsx
+++ packages/app/src/pages/DetailsPage.tsx
@@ -1,8 +1,8 @@
-import { ExportedResourceType } from '#components/Details/ExportedResourceType'
-import { ExportDetailsProvider } from '#components/Details/Provider'
-import { ExportDate } from '#components/Details/ExportDate'
-import { ErrorNotFound } from '#components/ErrorNotFound'
-import { appRoutes } from '#data/routes'
+import { ExportedResourceType } from "#components/Details/ExportedResourceType";
+import { ExportDetailsProvider } from "#components/Details/Provider";
+import { ExportDate } from "#components/Details/ExportDate";
+import { ErrorNotFound } from "#components/ErrorNotFound";
+import { appRoutes } from "#data/routes";
 import {
   useTokenProvider,
   PageSkeleton,
@@ -10,53 +10,53 @@ import {
   Spacer,
   Button,
   EmptyState,
-  useCoreSdkProvider
-} from '@commercelayer/app-elements'
-import { useLocation, useRoute, Link } from 'wouter'
-import { ExportReport } from '#components/Details/ExportReport'
-import { ExportDetails } from '#components/Details/ExportDetails'
+  useCoreSdkProvider,
+} from "@commercelayer/app-elements";
+import { useLocation, useRoute, Link } from "wouter";
+import { ExportReport } from "#components/Details/ExportReport";
+import { ExportDetails } from "#components/Details/ExportDetails";
 
 const DetailsPage = (): JSX.Element | null => {
   const {
     canUser,
-    settings: { mode }
-  } = useTokenProvider()
-  const { sdkClient } = useCoreSdkProvider()
-  const [_match, params] = useRoute(appRoutes.details.path)
-  const [_, setLocation] = useLocation()
+    settings: { mode },
+  } = useTokenProvider();
+  const { sdkClient } = useCoreSdkProvider();
+  const [_match, params] = useRoute(appRoutes.details.path);
+  const [_, setLocation] = useLocation();
 
-  const exportId = params == null ? null : params.exportId
+  const exportId = params == null ? null : params.exportId;
 
-  if (exportId == null || !canUser('read', 'exports')) {
+  if (exportId == null || !canUser("read", "exports")) {
     return (
       <PageLayout
-        title='Exports Eli test'
+        title="Exports"
         onGoBack={() => {
-          setLocation(appRoutes.list.makePath())
+          setLocation(appRoutes.list.makePath());
         }}
         mode={mode}
       >
         <EmptyState
-          title='Not authorized'
+          title="Not authorized"
           action={
             <Link href={appRoutes.list.makePath()}>
-              <Button variant='primary'>Go back</Button>
+              <Button variant="primary">Go back</Button>
             </Link>
           }
         />
       </PageLayout>
-    )
+    );
   }
 
   if (sdkClient == null) {
-    return <PageSkeleton layout='details' hasHeaderDescription />
+    return <PageSkeleton layout="details" hasHeaderDescription />;
   }
 
   return (
     <ExportDetailsProvider sdkClient={sdkClient} exportId={exportId}>
       {({ state: { isLoading, data } }) =>
         isLoading ? (
-          <PageSkeleton layout='details' hasHeaderDescription />
+          <PageSkeleton layout="details" hasHeaderDescription />
         ) : data == null ? (
           <ErrorNotFound />
         ) : (
@@ -66,30 +66,30 @@ const DetailsPage = (): JSX.Element | null => {
             description={
               <ExportDate
                 atType={
-                  data.status === 'completed' ? 'completed_at' : 'started_at'
+                  data.status === "completed" ? "completed_at" : "started_at"
                 }
                 prefixText={
-                  data.status === 'completed' ? 'Exported on ' : 'Started on'
+                  data.status === "completed" ? "Exported on " : "Started on"
                 }
                 includeTime
               />
             }
             onGoBack={() => {
-              setLocation(appRoutes.list.makePath())
+              setLocation(appRoutes.list.makePath());
             }}
           >
-            <Spacer bottom='12'>
+            <Spacer bottom="12">
               <ExportReport />
             </Spacer>
 
-            <Spacer bottom='12'>
+            <Spacer bottom="12">
               <ExportDetails />
             </Spacer>
           </PageLayout>
         )
       }
     </ExportDetailsProvider>
-  )
-}
+  );
+};
 
-export default DetailsPage
+export default DetailsPage;
